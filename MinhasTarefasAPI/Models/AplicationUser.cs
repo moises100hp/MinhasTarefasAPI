769dca7f1f09e8e6dc61c139de0f542d55380f26
Moises Silva <moises100hp@gmail.com>
2021-09-08 23:46:56 -0300

    Add project files.

---
+++ MinhasTarefasAPI/Models/AplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefasAPI.Models
+{
+    public class AplicationUser : IdentityUser
+    {
+        public string FullNamed { get; set; }
+        [ForeignKey("UsuarioId")]
+        public ICollection<Tarefa> Tarefas { get; set; }
+    }
+}
