8ee3e5255dcae490d29d5a505c64989f5bdab726
Moises Silva <moises100hp@gmail.com>
2021-09-09 22:53:20 -0300

    Adicionado banco e migrations

---
+++ MinhasTarefasAPI/Repositories/Contracts/IUsuarioRepository.cs
@@ -0,0 +1,16 @@
+﻿using MinhasTarefasAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefasAPI.Repositories.Contracts
+{
+    public interface IUsuarioRepository
+    {
+        void Cadastrar(AplicationUser usuario, string senha);
+        AplicationUser Obter(string email, string senha);
+
+
+    }
+}
